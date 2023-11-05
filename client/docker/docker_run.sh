77f7999022830ada0ca99c3da3df9c24b701fef0
Babbili <babbili@protonmail.com>
2023-11-05 14:29:20 +0400

    add k8s manifests

--- client/docker/docker_run.sh
+++ client/docker/docker_run.sh
@@ -2,14 +2,14 @@
 
 DOCKER_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-# cd $DOCKER_DIR && docker build -f Dockerfile --tag alpine ../
+# cd $DOCKER_DIR && docker build -f Dockerfile --tag alpine:c6707b6eb ../
 
-# docker scout cves --type image alpine
+# docker scout cves --type image alpine:c6707b6eb
 
-# docker run --name alpine-c alpine
+# docker run --name alpine-c alpine:c6707b6eb
 
-cd $DOCKER_DIR && docker build -f Dockerfile --tag babbili/alpine:c6702847 ../
+cd $DOCKER_DIR && docker build -f Dockerfile --tag babbili/alpine:c6707b6eb ../
 
-# docker scout cves --type image babbili/alpine:c6702847
+# docker scout cves --type image babbili/alpine:c6707b6eb
 
-docker push babbili/alpine:c6702847
+docker push babbili/alpine:c6707b6eb
